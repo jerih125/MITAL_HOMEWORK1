b220ac266912009a8c97d035f062dab20578a7b8
jerih125 <jerih125@gmail.com>
2015-10-24 02:51:44 +0700

    HomeWork1
    Cat

---
+++ HomeWork1/Model/Cat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        private string _name;
+        private int _health = 5;        
+        private string _age;
+        public CatColor Color { get; set; }
+
+        public string CurrentColor { get; set; }
+
+        public Cat(CatColor color)
+        {
+            Color = color;
+            CurrentColor = color.HealthyColor;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public string Age
+        {
+            get { return _age; }
+            set
+            {
+                if (string.IsNullOrEmpty(_age))
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public void Feed()
+        {
+            ++_health;
+        }
+
+        public void Punish()
+        {
+            --_health;
+        }
+
+        public void SetCurrentColor()
+        {
+            CurrentColor = _health < 5 ? Color.SickColor : Color.HealthyColor;
+        }
+    }
+}
---
+++ HomeWork1/View/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace View
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var color = new CatColor();
+            var Cat = new Cat(color);            
+
+            int i;
+            do
+            {
+                Cat.SetCurrentColor();
+                Console.WriteLine("Текущая информация о кошке.\nИмя: " + Cat.Name + "\nВозраст: " + Cat.Age + "\nТекущий цвет: " + Cat.CurrentColor);
+
+                Console.Write("\n\nМеню:\n1) Купить кота \n2) Задать цвет \n3) Наказать \n4) Покормить \n5) Выйти \n\nВаше решение: ");
+
+                i = int.Parse(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        Console.Write("Введите имя кошки: ");
+                        Cat.Name = Console.ReadLine();
+                        Console.Write("Введите возраст кошки: ");
+                        Cat.Age = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Введите цвет кошки: ");
+                        color.HealthyColor = Console.ReadLine();
+                        break;
+                    case 3:
+                        Cat.Punish();
+                        break;
+                    case 4:
+                        Cat.Feed();
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (i != 5);
+        }
+    }
+}
